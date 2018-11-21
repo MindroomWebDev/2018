7088dee4b677a9665d782f4de9485a6fd80df6fa
Vitor Bottazzi <codementorvitor@gmail.com>
2018-11-21 19:16:04 +1000

    Created template object instantiations and commented it waiting for the classes to be finished

--- index.php
+++ index.php
@@ -15,6 +15,20 @@ include 'car.php';
 echo '<h3>Lets list our cars here!</h3>';
 $car1 = new Car(5, True);
 echo "My first car have " . $car1->get_nr_of_pax() . " spots and the status for Auto is " . boolval($car1->get_is_auto());
+
+//$wheel1 = new Wheel(xxx,xxx);
+//echo "My first wheel have " . $wheel1->get_xxxxx() . " and  is " . $wheel1->get_yyyyy();
+
+//$door1 = new Door(xxx,xxx);
+//echo "My first door have " . $door1->get_xxxxx() . " and  is " . $door1->get_yyyyy();
+
+//$body1 = new Body(xxx,xxx);
+//echo "My first body have " . $body1->get_xxxxx() . " and  is " . $body1->get_yyyyy();
+
+//$engine1 = new Engine(xxx,xxx);
+//echo "My first engine have " . $engine1->get_xxxxx() . " and  is " . $engine1->get_yyyyy();
+
+
 ?>
 
 </body>
