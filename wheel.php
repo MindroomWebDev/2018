6b3fe13b5292ec60a79eade49963520191944301
Macbook Pro <MacbookPro@192-168-1-114.tpgi.com.au>
2018-11-21 19:40:12 +1000

    wheel additions

--- wheel.php
+++ wheel.php
@@ -1,7 +1,30 @@
 <?php
  class Wheel{
+   $diameter;
+   $width;
 
-   
+   function __construct($new_diameter, $new_width){
+     this->$diameter = $new_diameter;
+     this->$width = $new_width;
+   }
+
+   function set_width($new_width){
+     $this->$width = $new_width;
+   }
+
+   function get_width(){
+     return $this->$width;
+   }
+
+   function set_diameter($new_diameter){
+     $this->$diameter = $new_diameter;
+   }
+
+   function get_diameter(){
+     return $this->$diameter;
+   }
+
+
  }
 
 
